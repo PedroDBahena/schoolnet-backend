0d01bc140c9b73a70d12d5c543f5bfb1b7653d0d
pedro129 <pedrodeleonbahena26@gmail.com>
2022-05-13 20:41:51 -0500

    Create teachers module files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TeachersModule } from './modules/teachers/teachers.module';
+import { TeachersController } from './controllers/teachers/teachers.controller';
+import { TeachersService } from './services/teachers/teachers.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TeachersModule],
+  controllers: [AppController, TeachersController],
+  providers: [AppService, TeachersService],
 })
 export class AppModule {}
